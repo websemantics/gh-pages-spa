9e4fe3fd9a1517c588753d7503be608359e0f16d
Adnan <adnan@websemantics.ca>
2016-08-22 03:29:16 -0400

    Enable package to work from browser or node

--- README.md
+++ README.md
@@ -4,15 +4,51 @@
 ### [Live demo](http://websemantics.github.io/gh-pages-spa/)
 
 
+## Install
+
+Bower
+
+```
+bower install ghspa
+```
+
+NPM
+
+```
+npm install ghspa
+```
+
+
+## Getting Started
+
+1- Create a new project with two pages `index.html` and `404.html`.
+
+2- Include file `ghspa.js` in each page, as follows,
+
+```html
+<script type="text/javascript" src="path-to/ghspa.js"></script>
+```
+
+3- Set the title of `404.html` page to `404`, as follows,
+
+```html
+<title>404</title>
+```
+
+4- Optionally, set the global parameter `rootDomain` to `true` to indicate that the project has a custom domain, or is running on an organization repository, as follow,
+
+```html
+<script type="text/javascript">var rootDomain = true</script>
+```
+
+If the project runs on a public repository, set `rootDomain` to `false`, or ignore all together.
+
+
 ## Credits
 
-This project was inspired and built with these resources,
+This project was heavily inspired and built with these resources,
 
 [SPA Github Pages](https://github.com/rafrex/spa-github-pages), host single page apps with github pages.
-
-[S(GH)PA](https://www.smashingmagazine.com/2016/08/sghpa-single-page-app-hack-github-pages/), tutorial to build the ability to route paths for a GitHub Pages website to its index.html for handling as a single-page app (SPA).
-
-[SPA Template](https://github.com/csuwildcat/sghpa), Template for single page apps on Github Pages, via 404 meta redirect.
 
 [Pokemon Go](http://www.flaticon.com/packs/pokemon-go), a free SVG icon set.
 
--- ghspa.js
+++ ghspa.js
@@ -16,51 +16,133 @@
  /* Github Pagaes SPS package
   *
   * @param {Object} l, the document current location
-  * @param {Boolean} root, true if this project works without a repositoy name (custom domain, orginization repository)
+  * @param {Boolean} root, true if this project works without a repositoy name (custom domain, organization repository)
   */
 
- ;(function(l, root) {
+  ;
+  (function(root, factory) {
+      if (typeof define === 'function' && define.amd) {
+          define([],
+              function() {
+                  return (root.ghSPA = factory())
+              })
+      } else if (typeof module === 'object' && module.exports) {
+          module.exports = factory()
+      } else {
+          root.ghSPA = factory()
+      }
+  }(this, function() {
+      var root = this || global
+      var l = root.location
+      var me = { VERSION: '1.0.0'}
 
-   /* @param {String} repo, repository name if not working on root domain */
-   var repo = root ? '' : '/' + l.pathname.split('/')[1]
+      /* Library defaults, can be changed using the 'defaults' member method,
 
-  /**
-   * redirect() redirect all 404 trafic to index.html
-   *
-   * @return {Void}
-   */
+  		- debug (boolean), turns debug mode off / on
 
-   function redirect() {
-     l.replace(l.protocol + '//' + l.hostname + (l.port ? ':' + l.port : '') + repo + '/?' +
-              (l.pathname ? 'p=' + l.pathname.replace(/&/g, '~and~').replace(repo, '') : '') +
-              (l.search ? '&q=' + l.search.slice(1).replace(/&/g, '~and~') : '') +
-              (l.hash))
-   }
+  		- rootDomain (boolean), true to indicate that the project has a custom domain, or is running on an organization repository
 
-  /**
-   * resolve() resolve 404 redirects into internal routes
-   *
-   * @return {Void}
-   */
+    */
 
-   function resolve() {
-     if (l.search) {
-       var q = {}
-       l.search.slice(1).split('&').forEach(function(v) {
-         var a = v.split('=')
-         q[a[0]] = a.slice(1).join('=').replace(/~and~/g, '&')
-       })
-       if (q.p !== undefined) {
-         window.history.replaceState(null, null,
-           repo + (q.p || '') +
-           (q.q ? ('?' + q.q) : '') +
-           l.hash
-         )
+      var defaults = {
+          debug: true,
+          rootDomain: false,
+      }
+
+      // -------------------------------------------------------------------------
+      // Public methods
+
+      /*
+        Override class defaults
+
+          Parameters:
+          - opts (object): name value pairs
+
+      */
+
+      me.defaults = function(opts) {
+        var key
+        for(key in opts || {}){
+          if(defaults[key]){
+            defaults[key] = opts[key]
+          }
+        }
+      }
+
+      // -------------------------------------------------------------------------
+      // Public methods
+
+      /*
+      Clear cached data
+
+      */
+
+      me.clear = function() {
+          localStorage.removeItem(defaults.id);
+          log('Cache ({{id}}) has been cleared', {
+              id: defaults.id
+          })
+      }
+
+      // -------------------------------------------------------------------------
+      // Private methods
+
+
+       /* @param {String} repo, repository name if not working on root domain */
+       var defaults.rootDomain = root ? '' : '/' + l.pathname.split('/')[1]
+
+      /**
+       * redirect() redirect all 404 trafic to index.html
+       *
+       * @return {Void}
+       */
+
+       function redirect() {
+         l.replace(l.protocol + '//' + l.hostname + (l.port ? ':' + l.port : '') + repo + '/?' +
+                  (l.pathname ? 'p=' + l.pathname.replace(/&/g, '~and~').replace(repo, '') : '') +
+                  (l.search ? '&q=' + l.search.slice(1).replace(/&/g, '~and~') : '') +
+                  (l.hash))
        }
-     }
-   }
 
-   /* redirect all trafic to index.html if 404 page */
-   document.title === '404' ? redirect() : resolve()
+      /**
+       * resolve() resolve 404 redirects into internal routes
+       *
+       * @return {Void}
+       */
 
- }(window.location, window.rootDomain || false))
+       function resolve() {
+         if (l.search) {
+           var q = {}
+           l.search.slice(1).split('&').forEach(function(v) {
+             var a = v.split('=')
+             q[a[0]] = a.slice(1).join('=').replace(/~and~/g, '&')
+           })
+           if (q.p !== undefined) {
+             window.history.replaceState(null, null,
+               repo + (q.p || '') +
+               (q.q ? ('?' + q.q) : '') +
+               l.hash
+             )
+           }
+         }
+       }
+
+      /*
+            Log a message to the console
+
+              Parameters:
+      				- message (string): print out if in debug mode
+
+      */
+
+      function log(message) {
+          if (defaults.debug) {
+              console.log(message)
+          }
+      }
+
+      /* redirect all trafic to index.html if 404 page */
+      document.title === '404' ? redirect() : resolve()
+
+      return me
+  }))
